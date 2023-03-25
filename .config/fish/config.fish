9f5f636744858be61e370ce33625bfa22abc0230
Chinmai Trivedi <chinmaitrivedi93@gmail.com>
2023-03-25 00:41:43 -0400

    Add fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,54 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g theme_display_git no
+set -g theme_display_git_dirty no
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_default_branch yes
+set -g theme_git_default_branches master main
+set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_nix no
+set -g theme_display_ruby no
+set -g theme_display_node yes
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_vi no
+set -g theme_display_date no
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_color_scheme dark
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 1
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
+
+# Try to get pyenv name display on fish
+functions -c fish_prompt _old_fish_prompt
+function fish_prompt
+set pyenv_version (pyenv version-name | string split ":")
+ if set -q pyenv_version
+  echo -n -s (set_color -b blue white) "(" (basename "$pyenv_version") ")" (set_color normal) " "
+ end
+ _old_fish_prompt
+end
+
+# dotfiles
+alias mydotfiles='/usr/bin/git --git-dir=/home/chindog/.my-dotfiles/ --work-tree=/home/chindog'
